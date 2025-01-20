92f5a30826374964789b5a2bdabe2a838ba0e240
Mariam Samir <ms1143440@gmail.com>
2025-01-20 09:50:13 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./providers/router";
+import useAuth from "./hooks/helpers/useAuth";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { setIsLogged } from "./redux/slices/authedUserSlice";
 
 function App() {
-  return <RouterProvider router={router} />;
+  const { isAuthed, loading } = useAuth();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(setIsLogged(isAuthed));
+  }, [dispatch, isAuthed]);
+
+  return loading ? null : <RouterProvider router={router} />;
 }
 export default App;
