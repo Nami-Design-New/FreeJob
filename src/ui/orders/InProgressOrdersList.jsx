f4b1a4299c82000ba79281b2f1575757fdcb7a7b
Ahmedelsayed265 <ahmedelsayed2102@icloud.com>
2025-01-21 15:42:23 +0200

    refactor: simplify InProgressOrdersList component by removing pagination logic and integrating CustomPagination

--- src/ui/orders/InProgressOrdersList.jsx
+++ src/ui/orders/InProgressOrdersList.jsx
@@ -1,41 +1,20 @@
-import { useState } from "react";
 import InProgressCard from "../cards/InProgressCard";
-import PaginationComponent from "../PaginationComponent";
+import CustomPagination from "../CustomPagination";
 
-export default function InProgressOrdersList({
-  projectsOrdersList,
-  isLoading,
-}) {
-  const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 10;
-  const startIndex = (currentPage - 1) * itemsPerPage;
-  console.log(
-    projectsOrdersList.data.slice(startIndex, startIndex + itemsPerPage)
-  );
-  let currentProducts = projectsOrdersList.data.slice(
-    startIndex,
-    startIndex + itemsPerPage
-  );
-
-  const handlePageChange = (pageNumber) => {
-    setCurrentPage(pageNumber);
-  };
+export default function InProgressOrdersList({ projectsOrdersList }) {
   return (
     <section className="row g-4">
-      {currentProducts.map((order) => {
+      {projectsOrdersList?.data?.map((order) => {
         return (
-          <section key={order.id}>
+          <section key={order?.id}>
             <InProgressCard order={order} />
           </section>
         );
       })}
 
-      <PaginationComponent
-        totalItems={projectsOrdersList.total}
-        currentPage={currentPage}
-        onPageChange={handlePageChange}
-        itemsPerPage={itemsPerPage}
-      />
+      {projectsOrdersList && projectsOrdersList?.total > 10 && (
+        <CustomPagination count={projectsOrdersList?.total} pageSize={10} />
+      )}
     </section>
   );
 }
