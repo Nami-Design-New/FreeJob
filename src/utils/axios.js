67f31e776b2c794273cd677f109e83ca9a278c3c
Ahmedelsayed265 <ahmedelsayed2102@icloud.com>
2025-07-01 10:42:30 +0300

    fix: update base URL for axios instance from abday to ebday

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const lang = localStorage.getItem("lang") || "ar";
 
 const axiosInstance = axios.create();
-axiosInstance.defaults.baseURL = "https://api.abday.com.sa/api";
+axiosInstance.defaults.baseURL = "https://api.ebday.com.sa/api";
 axiosInstance.defaults.headers.common["Content-Type"] = "application/json";
 axiosInstance.defaults.headers.common["Accept"] = "application/json";
 axiosInstance.defaults.headers.common["lang"] = lang;
